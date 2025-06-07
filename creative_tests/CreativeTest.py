a52e53c9584d0b33f7d8dc605b06ad6ef03065b7
theGreen-Coder <greencode.org@gmail.com>
2025-06-07 19:51:55 +0200

    Small refactor to change LLM output

--- creative_tests/CreativeTest.py
+++ creative_tests/CreativeTest.py
@@ -1,5 +1,7 @@
-class DivergentTest():
-    def __init__(self, repetitions=1, models=[]):
+import abc
+
+class CreativeTest(abc.ABC):
+    def __init__(self, repetitions, models):
         self.repetitions = repetitions
         self.models = models
         self.test_prompt = ''
@@ -10,7 +12,7 @@ class DivergentTest():
     
     def set_test_prompt(self, test_prompt):
         self.test_prompt = test_prompt
-
+    
     def test(self):
         pass
 
--- creative_tests/DAT.py
+++ creative_tests/DAT.py
@@ -1,19 +1,23 @@
 import os
 from google import genai
 from google.genai import types
-from creative_tests.DivergentTest import DivergentTest
+from creative_tests.test import CreativeTest
 
-class DivergentAssociationTest(DivergentTest):
-    def __init__(self, repetitions=1, models=["google"]):
+class DivergentAssociationTest(CreativeTest):
+    def __init__(self, repetitions, models, embedding_models):
         super().__init__(repetitions, models)
+        self.embedding_models = embedding_models
+
         self.test_prompt = 'Please enter 10 words that are as different from each other as possible, ' \
                             'in all meanings and uses of the words. Rules: Only single words in English. ' \
                             'Only nouns (e.g., things, objects, concepts). No proper nouns (e.g., no specific people or places). ' \
                             'No specialized vocabulary (e.g., no technical terms). ' \
                             'Think of the words on your own (e.g., do not just look at objects in your surroundings). ' \
-                            'Make a list of these 10 words, a single word in each entry of the list.'
+                            'Make a list of these 10 words, a single word in each entry of the list.' \
+                            'Do not write anything else, but the 10 words.'
         
-        
+    def run(self):
+        pass
     
 
     
